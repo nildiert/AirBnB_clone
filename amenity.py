36d567e64ff45827a804554bebf12ff78e3aa6e8
javb92 <mentor37_@hotmail.com>
2019-06-27 23:58:15 +0000

    deleted remain files

--- amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" Amenity class """
-
-
-class Amenity(BaseModel):
-    """ This is the Amenity class """
-
-    name = ""
--- city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" City class """
-
-
-class City(BaseModel):
-    """ This is the city class """
-
-    state_id = ""
-    name = ""

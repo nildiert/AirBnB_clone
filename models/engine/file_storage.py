b5d0b6fae6a21a1008fb5413eeab355b7edab735
javb92 <mentor37_@hotmail.com>
2019-06-29 19:35:44 +0000

    fix bad name declaration

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage():
 
     def all(self):
         """ all method """
-#        print(__class__.__name__)
+        print(self.__objects, "all method")
         return self.__objects
 
     def new(self, obj):
@@ -24,10 +24,8 @@ class FileStorage():
 
     def reload(self):
         """ Reload method """
-        values = []
-        data = ""
         try:
             with open(self.__file_path, "r") as f:
-                self.__object = json.load(f)
+                self.__objects = json.load(f)
         except:
             pass

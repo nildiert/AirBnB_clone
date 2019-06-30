9ef5720a0ac13ac2c4f926607936101252694033
nildiert <niljordan23@gmail.com>
2019-06-30 02:27:01 +0000

    [FEATURE] Added function create, show and all in the console

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Import cmd """
 import cmd
 from models.base_model import BaseModel
+from models.city import City
 from models import storage
 import sys
 import inspect
@@ -13,8 +14,9 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ create method """
+
         if len(args) is 0:
-            print("** class name missing **")
+            HBNBCommand.error_handler(1)
         else:
             arguments = args.split()
             if HBNBCommand.verifyclass(arguments[0]):
@@ -22,54 +24,61 @@ class HBNBCommand(cmd.Cmd):
                 print(new_object.id)
                 new_object.save()
             else:
-                print("** class doesn't exist **'")
-
-#        super().__init__(args)
+                HBNBCommand.error_handler(2)
 
     def do_show(self, args):
         """ show method """
-        if len(args) is 0:
-            print("** class name missing **")
-        elif len(args) is 1:
-            print("** instance id missing **")
+        arguments = args.split()
+        if len(arguments) is 0:
+            HBNBCommand.error_handler(1)
+        elif len(arguments) is 1:
+            HBNBCommand.error_handler(4)
         else:
-            arguments = args.split()
             if HBNBCommand.verifyclass(arguments[0]):
                 storage.reload()
                 element = arguments[0] + "." + arguments[1]
                 if element in list(storage.all().keys()):
-                    print(BaseModel(storage.all()[element]))
+                    print(BaseModel(**storage.all()[element]))
                 else:
-                    print("** no instance found **")
+                    HBNBCommand.error_handler(2)
             else:
-                print("** class doesn't exist **'")
+                HBNBCommand.error_handler(2)
 
     def do_destroy(self, args):
         """ destroy method """
-        if len(args) is 0:
-            print("** class name missing **")
-        elif len(args) is 1:
-            print("** instance id missing **")
+        arguments = args.split()
+        if len(arguments) is 0:
+            HBNBCommand.error_handler(1)
+        elif len(arguments) is 1:
+            HBNBCommand.error_handler(4)
         else:
-            arguments = args.split()
+
             if HBNBCommand.verifyclass(arguments[0]):
                 storage.reload()
-                print(storage.all())
                 element = arguments[0] + "." + arguments[1]
                 if element in list(storage.all().keys()):
                     del storage.all()[element]
                     storage.save()
                 else:
-                    print("** no instance found **")
+                    HBNBCommand.error_handler(2)
             else:
-                print("** class doesn't exist **'")
+                HBNBCommand.error_handler(2)
 
-    def do_all():
+    def do_all(self, args):
         """ all method """
-        pass
+        if len(args) is 0:
+            HBNBCommand.error_handler(1)
+        else:
+            arguments = args.split()
+            if HBNBCommand.verifyclass(arguments[0]):
+                storage.reload()
+                for key, values in storage.all().items():
+                    if arguments[0] == key.split(".")[0]:
+                        print(BaseModel(**storage.all()[key]))
+            else:
+                HBNBCommand.error_handler(2)
 
-    def do_update():
-        """ update method """
+    def do_update(self, args):
         pass
 
     def do_EOF(self, *args):
@@ -92,6 +101,16 @@ class HBNBCommand(cmd.Cmd):
             return True
         else:
             return False
+
+    def error_handler(num_error):
+        if num_error is 1:
+            print("** class name missing **")
+        elif num_error is 2:
+            print("** class doesn't exist **'")
+        elif num_error is 3:
+            print("** no instance found **")
+        else:
+            print("** instance id missing **")
 
 
 if __name__ == '__main__':
